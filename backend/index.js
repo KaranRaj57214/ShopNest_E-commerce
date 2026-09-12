ddb8ca5954e7cd5c5fbf8cd944c57b5df9f586a4
Karan Raj <karan8799742199@gmail.com>
2026-09-12 17:55:51 +0530

    hello1

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db");
 
 dotenv.config();
@@ -24,11 +25,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Backend test route
-app.get("/", (req, res) => {
-  res.send("ShopNest Backend is running");
-});
-
 // API routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes.js"));
@@ -36,12 +32,86 @@ app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/payment", require("./routes/paymentRoutes.js"));
 app.use("/api/analytics", require("./routes/analyticsRoutes"));
 
+// Serve React frontend in production
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// React Router fallback
+// app.get("*", (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "../frontend/build", "index.html")
+//   );
+// });
+
+app.use((req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build", "index.html")
+  );
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const connectDB = require("./config/db");
+
+// dotenv.config();
+
+// connectDB();
+
+// const app = express();
+
+// // CORS
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "http://127.0.0.1:3000",
+//       process.env.FRONTEND_URL,
+//     ],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // Backend test route
+// app.get("/", (req, res) => {
+//   res.send("ShopNest Backend is running");
+// });
+
+// // API routes
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/products", require("./routes/productRoutes.js"));
+// app.use("/api/orders", require("./routes/orderRoutes"));
+// app.use("/api/payment", require("./routes/paymentRoutes.js"));
+// app.use("/api/analytics", require("./routes/analyticsRoutes"));
+
+// // Start server
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 
 
